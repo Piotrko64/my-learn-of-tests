52a1bfdffbfe3ac340d4cba6c992886f8a81b908
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-05-18 22:12:05 +0200

    refactorization

--- basic-testing-frontend/src/util/numbers.spec.js
+++ basic-testing-frontend/src/util/numbers.spec.js
@@ -1,39 +1,61 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import { clearNumbers, transformToNumber } from "./numbers";
 
-it("Check transform string to number - string is a number", () => {
-    // Arrange
-    const numberAsString = "64";
+describe("transform to number", () => {
+    it("Check transform string to number - string is a number", () => {
+        // Arrange
+        const numberAsString = "64";
 
-    // Act
-    const result = transformToNumber(numberAsString);
+        // Act
+        const result = transformToNumber(numberAsString);
 
-    // Assert
-    const expectResult = +numberAsString;
+        // Assert
+        const expectResult = +numberAsString;
 
-    expect(result).toBe(expectResult).toBeTypeOf("number").not.toBeNaN();
+        expect(result).toBe(expectResult).toBeTypeOf("number").not.toBeNaN();
+    });
+
+    it("Check transform string to number - string is a word", () => {
+        // Arrange
+        const simpleString = "I am simple but invalid!";
+        // const exampleWithArray = [0, 1];
+
+        // Act
+        const result = transformToNumber(simpleString);
+        // const resultArray = transformToNumber(exampleWithArray);
+
+        // Assert
+        expect(result).toBeNaN();
+        // expect(resultArray).toBeNaN();
+    });
+
+    // Vitest coverage is awesome tool!
+
+    it("should not throw error and transform show  array numbers", () => {
+        const valueArray = ["3", "2", "11"];
+
+        const result = clearNumbers(valueArray);
+
+        expect(result).toEqual(valueArray.map((el) => +el));
+    });
 });
 
-it("Check transform string to number - string is a word", () => {
-    // Arrange
-    const simpleString = "I am simple but invalid!";
-    // const exampleWithArray = [0, 1];
+describe("clearNumbers()", () => {
+    // Integration test
+    it("should return an array of number array if an array strin is provided", () => {
+        const arrNumbers = ["1", "2"];
 
-    // Act
-    const result = transformToNumber(simpleString);
-    // const resultArray = transformToNumber(exampleWithArray);
+        const cleanedNumbers = clearNumbers(arrNumbers);
 
-    // Assert
-    expect(result).toBeNaN();
-    // expect(resultArray).toBeNaN();
-});
+        expect(cleanedNumbers[0]).toBeTypeOf("number");
+    });
+    it("should throw an error - array with least one empty string", () => {
+        const numberVal = ["", " ", 3];
 
-// Vitest coverage is awesome tool!
+        const cleanFn = () => {
+            clearNumbers(numberVal);
+        };
 
-it("should not throw error and transform show  array numbers", () => {
-    const valueArray = ["3", "2", "11"];
-
-    const result = clearNumbers(valueArray);
-
-    expect(result).toEqual(valueArray.map((el) => +el));
+        expect(cleanFn).toThrow();
+    });
 });
