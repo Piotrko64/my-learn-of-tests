1189e2dcd12ed4d5992bf8bfa5644ee65b618f50
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-05-18 08:42:56 +0200

    provide AAA pattern

--- basic-testing-frontend/src/math.spec.js
+++ basic-testing-frontend/src/math.spec.js
@@ -1,1 +1,13 @@
-import { test } from "vitest"; //or {it}
+import { it, expect } from "vitest"; //or {test}
+import { add } from "./math";
+
+it("should sumarize all numbers values from array", () => {
+    // ARRANGE
+    const numbers = [1, 2, 3];
+
+    // ACT
+    const result = add(numbers);
+
+    // ASSERT
+    expect(result).toBe(6);
+});
