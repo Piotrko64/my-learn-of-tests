a6ca8b74cf2bf6c37b10b005f3eb35f15f85ab8c
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-05-18 12:48:53 +0200

    use describe() to add description for tests

--- basic-testing-frontend/src/util/validation.spec.js
+++ basic-testing-frontend/src/util/validation.spec.js
@@ -1,27 +1,39 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { validateNumber, validateStringNotEmpty } from "./validation";
 
-it("Input should be not empty", () => {
-    const inputString = " Tests are awesome! ";
-    const inputEmptyString = " ";
+describe("validateStringNotEmpty", () => {
+    it("Input should be not empty", () => {
+        const inputString = " Tests are awesome! ";
+        const inputEmptyString = " ";
 
-    const resultCheckLength = () => validateStringNotEmpty(inputString);
-    const resultCheckLengthEmptyString = () => validateStringNotEmpty(inputEmptyString);
+        const resultCheckLength = () => validateStringNotEmpty(inputString);
+        const resultCheckLengthEmptyString = () => validateStringNotEmpty(inputEmptyString);
 
-    expect(resultCheckLength).not.toThrowError();
-    expect(resultCheckLengthEmptyString).toThrowError(/must not be empty./);
+        expect(resultCheckLength).not.toThrowError();
+        expect(resultCheckLengthEmptyString).toThrowError(/must not be empty./);
+    });
 });
 
-it("should be not NaN", () => {
-    const randomNaN = "64P"; // or {} or "['a']";
+describe(" Testing validationNumber", () => {
+    it("should throw Error", () => {
+        const randomNaN = "64P"; // or {} or "['a']";
 
-    const resultNaN = () => validateNumber(randomNaN);
+        const resultNaN = () => validateNumber(randomNaN);
 
-    expect(resultNaN).toThrowError(/Invalid number input/);
-});
+        expect(resultNaN).toThrowError(/Invalid number input/);
+    });
 
-it("should not throw error", () => {
-    const randomNumber = 555_444_333;
-    const resultNumber = () => validateNumber(randomNumber);
-    expect(resultNumber).not.toThrowError();
+    it("should thro Error - input is NaN", () => {
+        const NaNvalue = NaN;
+
+        const resultNaN = () => validateNumber(NaNvalue);
+
+        expect(resultNaN).toThrowError(/Invalid number input/);
+    });
+
+    it("should not throw error", () => {
+        const randomNumber = 555_444_333;
+        const resultNumber = () => validateNumber(randomNumber);
+        expect(resultNumber).not.toThrowError();
+    });
 });
